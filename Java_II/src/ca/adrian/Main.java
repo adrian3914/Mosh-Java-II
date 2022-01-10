d79f771ae65b4ca046c0769b70a2783938104a3b
Adrian Paz Alonso <adrian.pa31@outlook.com>
2022-01-10 18:01:09 -0500

    Constructors added to parent UIControl and child class TextBox using the super keyword

--- Java_II/src/ca/adrian/Main.java
+++ Java_II/src/ca/adrian/Main.java
@@ -3,7 +3,7 @@ package ca.adrian;
 public class Main {
 
     public static void main(String[] args) {
-
+        var textBox = new TextBox();
     }
 
 
--- Java_II/src/ca/adrian/TextBox.java
+++ Java_II/src/ca/adrian/TextBox.java
@@ -4,6 +4,11 @@ public class TextBox extends UIControl{
     // fields
     private String text = "";
 
+    public TextBox() {
+        super(true);
+        System.out.println("TextBox");
+    }
+
     // methods
      public void setText(String text){
         this.text = text; // this -> this object
